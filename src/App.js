a3e92a8dcb0f3deb4e399f26c17a6c03bd550ff7
AriffFakhri <arifffakhri@gmail.com>
2023-11-28 15:56:11 +0800

    day 6 task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./App.css"
+import Home from './Home'
+import Navbar from './Navbar'
+import Rooms from './Rooms'
+import Activity from './Activity'
+import Aboutus from './Aboutus'
+import Footer from './Footer'
+import Contactus from './Contactus'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='room' element={<Rooms/>}/>
+        <Route path='xtvt' element={<Activity/>}/>
+        <Route path='abt' element={<Aboutus/>}/>
+        <Route path='cont' element={<Contactus/>}/>
+
+      </Routes>
+
+      <Footer/>
+    </Router>
+    
+  </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1 style={{color:"gray", textAlign:"center"}}>Welcome To Hanana Chalet Page</h1>
+        <p>Nestled amidst the concrete jungle of Kuala Lumpur, <strong>Hanana Chalet</strong> is an unexpected heaven offering a full experience of sophisticated style, comfort, privacy and tranquility.
+
+          Our 30 rooms resort hotel overlooks the scenic 150-acre lake, providing the perfect escape from the hectic city life, located just 30 minutes away from Kuala Lumpur City Centre. It is a place where you look for a little piece of serene nature within the bustling city.
+
+          Strategically located within a 1,000-acre mixed development just 35 minutes south of Kuala Lumpur’s City Centre, we are just a 65-minutes drive from KLIA 1 and 2, Putrajaya and Cyberjaya.</p>
+        <img src='pic1.jpg' style={{width:"100%", margin:"10px auto", height:"600px"}}/>
+
+    </div>
+    
+  )
+}
+
+export default Home

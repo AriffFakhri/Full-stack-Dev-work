4139be0b137ea8f33c023ba45c7261739bf6f62a
AriffFakhri <149296889+AriffFakhri@users.noreply.github.com>
2023-12-09 19:51:01 +0800

    Add files via upload

---
+++ DAY 11 ASSIGNMENT 2/components/Assignment.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+      <form @submit.prevent="getdata">
+        <h1>Form</h1>
+        <label>Your Email</label><br>
+        <input type="text" placeholder="Enter Your Email" v-model="email" /><br><br>
+        
+        <label>Password</label><br>
+        <input type="password" placeholder="Enter password" v-model="password" /><br><br>
+        
+        <label>Your IC</label><br>
+        <input type="text" placeholder="Enter Your IC" v-model="ic" /><br><br>
+        
+        <button type="submit">Get Values</button><br><br>
+      </form>
+  
+      <div>
+        <table v-if="data">
+          <tr>
+            <th>Email</th>
+            <th>Password</th>
+            <th>IC</th>
+          </tr>
+          <tr>
+            <td>{{ data.email }}</td>
+            <td>{{ data.password }}</td>
+            <td>{{ data.ic }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Assignment",
+    data() {
+      return {
+        email: null,
+        password: null,
+        ic: null,
+      
+      };
+    },
+    methods: {
+      getdata() {
+        this.data = {
+          email: this.email,
+          password: this.password,
+          ic: this.ic,
+        };
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: turquoise;
+  }
+  form {
+    text-align: center;
+  }
+  </style>
---
+++ DAY 11 ASSIGNMENT 2/components/AssignmentTwo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Counter">
+    <h1>Vue Counter</h1>
+    <p>Current Count: {{ count }}</p>
+    
+    <br><br>
+    
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AssignmentTwo",
+  data() {
+    return {
+      count: 0,
+      
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+      
+    },
+    decrement() {
+      this.count--;
+    }
+  }
+};
+</script>
+
+<style>
+/* Your styles go here */
+</style>

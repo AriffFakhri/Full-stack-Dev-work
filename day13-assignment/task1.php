9ee66f18d0f2773c402211f1c019db2a546cd40d
AriffFakhri <149296889+AriffFakhri@users.noreply.github.com>
2023-12-16 18:01:21 +0800

    Add files via upload

---
+++ day13-assignment/task1.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$age =25;
+
+if ($age >= 25 && $age <65){
+    echo "you are eligible to drive";
+}elseif ($age >= 65){
+    echo "you are too old to drive";
+}else {
+    echo "you are too young to drive";
+} 
+?>
